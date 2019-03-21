2e261089ea782da2b61e1eaaf610987a93c5e468
ezeq10 <eclipsejem@gmail.com>
2019-03-21 15:42:50 -0300

    Added backend

---
+++ backend/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database
+mongoose.connect('mongodb://127.0.0.1/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+// Models
+const TodoModel = require('./models/todo-model');
+
+// Routes
+const todoRoutes = express.Router();
+app.use('/todos', todoRoutes);
+
+todoRoutes.route('/').get((req, res) => {
+  TodoModel.find((err, todos) => {
+    return (err) 
+      ? console.log(err)
+      : res.json(todos)
+  });
+});
+    
+todoRoutes.route('/:id').get((req, res) => {
+  let id = req.params.id;
+  TodoModel.findById(id, (err, todo) => {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route('/add').post((req, res) => {
+  let todo = new TodoModel(req.body);
+  todo.save()
+    .then(todo => {
+      res.status(200).json({'todo': 'todo added successfully'});
+    })
+    .catch(err => {
+      res.status(400).send('adding new todo failed');
+    });
+});
+
+todoRoutes.route('/update/:id').post((req, res) => {
+  TodoModel.findById(req.params.id, (err, todo) => {
+      if (!todo) {
+        res.status(404).send("data is not found");
+      }       
+      else {      
+        todo.todoDescription = req.body.todoDescription;
+        todo.todoResponsible = req.body.todoResponsible;
+        todo.todoPriority = req.body.todoPriority;
+        todo.todoCompleted = req.body.todoCompleted;
+
+        todo.save()
+          .then(todo => {
+            res.json('Todo updated!');
+          })
+          .catch(err => {
+            res.status(400).send("Update not possible");
+          });
+      }  
+  });
+});
+
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+  console.log('Server listening on port ' + port);
+});
--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,11 +1,121 @@
 import React, { Component } from 'react';
 
 export default class CreateTodo extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      todoDescription: '',
+      todoResponsible: '',
+      todoPriority: '',
+      todoCompleted: false    
+    };
+
+    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeTodoDescription (e) {
+    e.preventDefault();
+    this.setState({
+      todoDescription: e.target.value
+    });
+  }
+
+  onChangeTodoResponsible (e) {
+    e.preventDefault();
+    this.setState({
+      todoResponsible: e.target.value
+    });
+  }
+
+  onChangeTodoPriority (e) {
+    e.preventDefault();
+    this.setState({
+      todoPriority: e.target.value
+    });
+  }
+
+  onSubmit (e) {
+    e.preventDefault();
+
+    console.log(`Form submitted:`);
+    console.log(`Todo Description: ${this.state.todoDescription}`);
+    console.log(`Todo Responsible: ${this.state.todoResponsible}`);
+    console.log(`Todo Priority: ${this.state.todoPriority}`);
+    
+    this.setState({
+      todoDescription: '',
+      todoResponsible: '',
+      todoPriority: '',
+      todoCompleted: false
+    });
+  }
+
   render() {
     return (
-      <div>
-        <p>Welcome to Create Component!!</p>
-      </div>
+      <div style={{marginTop: 10}}>
+        <h3>Create New Todo</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Description: </label>
+            <input type="text"
+                    className="form-control"
+                    value={this.state.todoDescription}
+                    onChange={this.onChangeTodoDescription}
+                    />
+          </div>
+          <div className="form-group">
+              <label>Responsible: </label>
+              <input type="text" 
+                      className="form-control"
+                      value={this.state.todoResponsible}
+                      onChange={this.onChangeTodoResponsible}
+                      />
+          </div>
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" 
+                      type="radio" 
+                      name="priorityOptions" 
+                      id="priorityLow" 
+                      value="Low"
+                      checked={this.state.todoPriority==='Low'} 
+                      onChange={this.onChangeTodoPriority}
+                      />
+                <label className="form-check-label">Low</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" 
+                      type="radio" 
+                      name="priorityOptions" 
+                      id="priorityMedium" 
+                      value="Medium" 
+                      checked={this.state.todoPriority==='Medium'} 
+                      onChange={this.onChangeTodoPriority}
+                      />
+                <label className="form-check-label">Medium</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" 
+                      type="radio" 
+                      name="priorityOptions" 
+                      id="priorityHigh" 
+                      value="High" 
+                      checked={this.state.todoPriority==='High'} 
+                      onChange={this.onChangeTodoPriority}
+                      />
+                <label className="form-check-label">High</label>
+            </div>
+        </div>
+          <div className="form-group">
+            <input type="submit" value="Create Todo" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>    
     )
   }
 }
